81fdb1081440aeb1da8c7ee13bb683d864f613d9
dgo <jdiegogarciaco@gmail.com>
2023-09-20 15:06:44 +0200

    replace home nav

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,9 +1,10 @@
-import { useRoutes } from "react-router-dom";
+import { Navigate, useRoutes } from "react-router-dom";
 import { Dashboard } from "../pages/Dashboard";
 
 export const Routes = () => {
     const element = useRoutes([
-        {path: "dashboard", element: <Dashboard />}
+        {path: "dashboard", element: <Dashboard />},
+        {path: "/", element: <Navigate to='/dashboard' replace />}
     ])
   return element;
 }
