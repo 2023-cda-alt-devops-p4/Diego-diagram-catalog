4fc3daf2f0a08901c8544e0678f60eaf4bcce838
dgo <jdiegogarciaco@gmail.com>
2023-09-20 11:21:28 +0200

    define base config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/2023-cda-alt-devops-p4/catalog-dg/',
+  base: '/catalog-dg/',
   plugins: [react()],
 })
