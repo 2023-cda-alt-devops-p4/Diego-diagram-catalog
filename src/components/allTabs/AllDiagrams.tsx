41fabbe9b160036e5c348d8f8e044e9f99525ddd
dgo <jdiegogarciaco@gmail.com>
2023-09-22 10:39:40 +0200

    feat: add model card

--- src/components/allTabs/AllDiagrams.tsx
+++ src/components/allTabs/AllDiagrams.tsx
@@ -1,6 +1,19 @@
+import { diagrams }from "../../data/diagrams";
+import { DiagramCard } from "../DiagramCard";
+import { ModelCard } from "../ModelCard";
+import { umlDiagrams } from "../../data/UMLdiagrams";
 
 export const AllDiagrams = () => {
+  const umlData = diagrams.filter((UMLId) => UMLId.id === 1)[0];
+  
   return (
-    <div>AllDiagrams</div>
+    <div>
+      <ModelCard data={umlData} />
+      {
+        umlDiagrams.map(umlDiagram => (
+          <DiagramCard data={umlDiagram} />
+        ))
+      }
+    </div>
   )
 }
