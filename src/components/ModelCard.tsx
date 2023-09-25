e12c8df648a5452c2c0067e93b37e68b06ee5bd4
dgo <jdiegogarciaco@gmail.com>
2023-09-25 14:43:37 +0200

    style: add uml and merise images to main page

--- src/components/ModelCard.tsx
+++ src/components/ModelCard.tsx
@@ -6,22 +6,22 @@ type ModelCardProps = {
 };
 
 export const ModelCard = ({ data }: ModelCardProps) => {
-  console.log("here", data);
-
   return (
     <div className={styles.modelContainer}>
-      <img
-        src={data.image}
-        alt={`${data.title} image`}
-        className={styles.modelImage}
-      />
+      <div className={styles.imgParent}>
+        <img
+          src={data.image}
+          alt={`${data.title} image`}
+          className={styles.modelImage}
+        />
+      </div>
       <h1>{data.title}</h1>
       <p>{data.description}</p>
       <div className={styles.softwareContainer}>
         Logiciels:
-      {data.software.map((eachOne) => (
-        <p className={styles.software}>{eachOne}</p>
-      ))}
+        {data.software.map((eachOne) => (
+          <p className={styles.software}>{eachOne}</p>
+        ))}
       </div>
     </div>
   );
