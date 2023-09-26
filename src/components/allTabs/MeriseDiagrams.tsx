a9453379990e04f3c69ba2e159a8489e14852bb9
dgo <jdiegogarciaco@gmail.com>
2023-09-26 18:10:53 +0200

    chore: pass json data to components

--- src/components/allTabs/MeriseDiagrams.tsx
+++ src/components/allTabs/MeriseDiagrams.tsx
@@ -1,11 +1,13 @@
-import { meriseDiagrams } from "../../data/merise";
+import data from "../../data/diagrams.json";
+import { IDiagram } from "../../models/diagram";
 import { Tab } from "../Tab";
 
 export const MeriseDiagrams = () => {
+  const meriseDiagrams = data.find(x => x.type === 'Merise')?.allDiagrams;
   return (
     <Tab
     tabTitle="Diagrammes Merise"
-    dataToDisplay={meriseDiagrams}
+    dataToDisplay={meriseDiagrams as IDiagram[]}
     parentRoute="merise-diagrams"
     essentials={false}
     />
--- src/components/allTabs/UMLDiagrams.tsx
+++ src/components/allTabs/UMLDiagrams.tsx
@@ -1,11 +1,13 @@
-import { umlDiagrams } from "../../data/UMLdiagrams";
+import data from "../../data/diagrams.json";
+import { IDiagram } from "../../models/diagram";
 import { Tab } from "../Tab";
 
 export const UMLDiagrams = () => {
+  const umlDiagrams = data.find(x => x.type === 'UML')?.allDiagrams;
   return (
     <Tab
       tabTitle="Diagrammes UML"
-      dataToDisplay={umlDiagrams}
+      dataToDisplay={umlDiagrams as IDiagram[]}
       parentRoute="uml-diagrams"
       essentials={false}
     />
