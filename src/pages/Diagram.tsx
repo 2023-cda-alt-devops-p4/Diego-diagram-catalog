6de387a103194b07e96e6227a9947680cfa16c69
dgo <jdiegogarciaco@gmail.com>
2023-09-30 18:48:10 +0200

    style: center content

--- src/pages/Diagram.tsx
+++ src/pages/Diagram.tsx
@@ -1,5 +1,5 @@
 import { useLocation, useParams } from "react-router-dom";
-import data from "../data/diagrams.json"
+import data from "../data/diagrams.json";
 import { IDiagram } from "../models/diagram";
 import { useEffect, useState } from "react";
 import styles from "./Diagram.module.css";
@@ -9,13 +9,15 @@ export const Diagram = () => {
   const { pathname } = useLocation();
   const [dataToDisplay, setDataToDisplay] = useState<IDiagram | null>(null);
 
-  const meriseDiagrams = data.find(x => x.type === 'Merise')?.allDiagrams;
-  const umlDiagrams = data.find(x => x.type === 'UML')?.allDiagrams;
+  const meriseDiagrams = data.find((x) => x.type === "Merise")?.allDiagrams;
+  const umlDiagrams = data.find((x) => x.type === "UML")?.allDiagrams;
 
   const filterDataById = (dataToFilter: IDiagram[]) => {
-      const result = dataToFilter.filter((x) => x.id === Number(params.id?.replace(/^\D+/g, '')))[0];
-      return result;
-  }
+    const result = dataToFilter.filter(
+      (x) => x.id === Number(params.id?.replace(/^\D+/g, ""))
+    )[0];
+    return result;
+  };
 
   const getDiagramData = () => {
     if (pathname.includes("/merise")) {
@@ -34,14 +36,24 @@ export const Diagram = () => {
   if (dataToDisplay === null) return;
 
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>{dataToDisplay!.title}</h1>
-      <img src={dataToDisplay.image} alt={`${dataToDisplay.title} image`} className={styles.image} />
-      <div className={styles.textContainer}>
-      <p className={styles.paragraph}>{dataToDisplay.largeDescription}</p>
-      <a href={dataToDisplay.source} target="_blank" className={styles.sourceLink}>
-        Read more
-      </a>
+    <div className={styles.box}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>{dataToDisplay!.title}</h1>
+        <img
+          src={dataToDisplay.image}
+          alt={`${dataToDisplay.title} image`}
+          className={styles.image}
+        />
+        <div className={styles.textContainer}>
+          <p className={styles.paragraph}>{dataToDisplay.largeDescription}</p>
+          <a
+            href={dataToDisplay.source}
+            target="_blank"
+            className={styles.sourceLink}
+          >
+            Read more
+          </a>
+        </div>
       </div>
     </div>
   );
